8b61ef9c6b6d1ffa487362f8de61d6d6d05a025b
ituki0426 <yuzurihainori315@gmail.com>
2024-10-09 05:22:52 +0900

    add new apo

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+import re
 
 app = FastAPI()
+
+@app.get("/scrape")
+async def scrape():
+    url = "https://www.ibaraki-ct.ac.jp/info/archives/65544"
+    date_pattern = r"\d{1,2}／\d{1,2}（.*?）"
+    # Webページの内容を取得
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    paragraphs = soup.find_all('p')
+    new_list = []
+    for p in paragraphs:
+        if re.search(date_pattern, p.get_text()):
+            new_list.append(p.get_text())
+
+    print(new_list)
+    return {"result": new_list}
 
 @app.get("/")
 async def root():
